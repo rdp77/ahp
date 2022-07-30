6826963b18f9f4be039c2b272d50e24cbea817ab
Moh Ravi Dwi Putra <ravidwiputra@gmail.com>
2022-07-31 00:03:51 +0700

    Update Changes

--- database/migrations/2022_07_30_223317_create_major_table.php
+++ database/migrations/2022_07_30_223317_create_major_table.php
@@ -16,7 +16,7 @@ class CreateMajorTable extends Migration
         Schema::create('major', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->foreign('faculty_id')->references('id')->on('faculty');
+            $table->foreignId('faculty_id')->constrained('faculty');
             $table->timestamps();
         });
     }
