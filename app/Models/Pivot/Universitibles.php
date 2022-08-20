cc4e13620a293f3d70f0774594b3d52f66cceb3a
Moh Ravi Dwi Putra <ravidwiputra@gmail.com>
2022-08-20 13:35:02 +0700

    Update Changes

---
+++ app/Models/Pivot/Universitibles.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models\Pivot;
+
+use Illuminate\Database\Eloquent\Relations\MorphPivot;
+
+class Universitibles extends MorphPivot
+{
+    public $table = "universitibles";
+}
